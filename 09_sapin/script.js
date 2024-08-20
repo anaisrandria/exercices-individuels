bcf94793377a4b72f7aa3c575811c71719ad630b
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-08-20 23:22:23 +0200

    Add pointe sapin

--- 09_sapin/script.js
+++ 09_sapin/script.js
@@ -1,28 +1,43 @@
-let etoiles = "";
-let espaces = "";
+
 
 // MAIN FUNCTION
-function afficherEtoiles(n) {
-    afficherTriangleDroite(n);
-    etoiles = "";
-    afficherTriangleGauche(n);
-}
+// function afficherEtoiles(n) {
+//     afficherTriangleDroite(n);
+//     etoiles = "";
+//     afficherTriangleGauche(n);
+// }
 
-afficherEtoiles(5);
+// afficherEtoiles(5);
 
 // SECONDARY FUNCTIONS
-function afficherTriangleDroite(n) {
+// function afficherTriangleDroite(n) {
+//     for (let i = 0; i < n; i++) {
+//         console.log(`${etoiles}\\`);
+//         etoiles += "*";
+//     }
+// }
+
+// function afficherTriangleGauche(n) {
+//     for (let j = 0; j < n; j++) {
+//         espaces = " ".repeat(n-j);
+//         console.log(`${espaces}/${etoiles}`);
+//         etoiles += "*";
+//     } 
+
+// }
+
+function afficherSapin(n) {
+    let etoiles = "";
+    let espaces = "";
+
+    espaces = " ".repeat(n+1);
+    console.log(`${espaces}+`);
+
     for (let i = 0; i < n; i++) {
-        console.log(`${etoiles}\\`);
+        espaces = " ".repeat(n-i);
+        console.log(`${espaces}/${etoiles}|${etoiles}\\`);
         etoiles += "*";
-    }
+    }   
 }
 
-function afficherTriangleGauche(n) {
-    for (let j = 0; j < n; j++) {
-        espaces = " ".repeat(n-j);
-        console.log(`${espaces}/${etoiles}`);
-        etoiles += "*";
-    } 
-
-}
+afficherSapin(10)
