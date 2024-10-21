66e07fb81dab4b5b827baa5cb8b12df5ec955b61
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-10-21 19:57:57 +0200

    add: call functions for each class wip

--- 00_autres_exos/branche_poo/heritage.py
+++ 00_autres_exos/branche_poo/heritage.py
@@ -6,40 +6,76 @@ class Vaisseau:
         self.type = type
         self.size = size
 
-    def getFeatures(self)
-        print("Caractéristiques du vaisseau :", self.name, self.type, self.size)
-
+    def getFeatures(self):
+        print("Nom :", self.name)
+        print("Type :", self.type)
+        print("Taille :", self.size)
 
 # Création d'une classe enfant
 class Croiseur(Vaisseau): 
-    def __init__(self, name, type, size, capacity)
+    def __init__(self, name, type, size, capacity):
+        super().__init__(name, type, size)
         self.capacity = capacity
 
-    def chargerTroupes(self, capacity)
-        self.capacity += 1
+    def getCapacity(self):
+        print("Capacité :", self.capacity, "hommes")
 
-    def dechargerTroupes(self, capacity)
-        self.capacity -= 1
+    def chargerTroupes(self, capacity):
+        self.capacity += capacity
+        print("New capacity (+", capacity, "):", self.capacity, "hommes")
 
+    def dechargerTroupes(self, capacity):
+        self.capacity -= capacity
+        print("New capacity (-", capacity, "):", self.capacity, "hommes")
 
 # Création d'une classe enfant
 class Intercepteur(Vaisseau): 
-    def __init__(self, name, type, size, nb_canon)
-        self.canon = canon
+    def __init__(self, name, type, size, nb_canon):
+        super().__init__(name, type, size)
+        self.nb_canon = nb_canon
+    
+    def getCanons(self):
+        print("Nombre de canons :", self.nb_canon)
 
-    def tirer(self)
-        print("Tire !")
+    def tirer(self):
+        print("Action : Tire !")
 
-    def recharger(self)
-        print("Recharge !")
-
+    def recharger(self):
+        print("Action : Recharge !")
 
 # Instanciation de classe
 acclamator = Croiseur("Acclamator", "Croiseur", 752, 700)
 corvette = Croiseur("Corvette corellienne CR90", "Croiseur", 150, 165)
+xwing = Intercepteur("X-wing", "Intercepteur", 12.5, 2)
+ywing = Intercepteur("Y-wing", "Intercepteur", 23, 2)
 
+# Appel des méthodes de l'instance Acclamator de type Croiseur
 acclamator.getFeatures()
+acclamator.getCapacity()
+acclamator.chargerTroupes(10)
+acclamator.dechargerTroupes(10)
+print()
 
+# Appel des méthodes de l'instance Corvette de type Croiseur
+corvette.getFeatures()
+corvette.getCapacity()
+corvette.chargerTroupes(15)
+corvette.dechargerTroupes(2)
+print()
+
+# Appel des méthodes de l'instance X-Wing de type Intercepteur
+xwing.getFeatures()
+xwing.getCanons()
+xwing.tirer()
+xwing.recharger()
+print()
+
+# Appel des méthodes de l'instance Y-Wing de type Intercepteur
+ywing.getFeatures()
+ywing.getCanons()
+ywing.tirer()
+ywing.recharger()
+print()
 
 
 
